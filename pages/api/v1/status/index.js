3f62c43f4df42565e2c3cb12bdab47a3fee33ae2
Leonardo Pontin <leo.pontin2@gmail.com>
2025-01-13 06:44:51 -0300

    Subindo `database.js`

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  res.status(200).json({ status: "OK" });
+}
+
+export default status;
